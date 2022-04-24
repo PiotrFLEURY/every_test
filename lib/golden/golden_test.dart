3baa5b4c2dae6b9071d1cf2126b1b5a8b267ab38
PiotrFLEURY <piotr.fleury@gmail.com>
2022-04-24 15:28:45 +0200

    add documentation in code

--- lib/golden/golden_test.dart
+++ lib/golden/golden_test.dart
@@ -1,18 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 
+/// Callback type used to call the test code
 typedef EveryTestGoldenCallback = Future<void> Function(
   WidgetTester tester,
   dynamic parameter,
 );
 
+/// Symbolize a golden test finder, its parameter instance and its expected value
 class EveryTestFinder {
+  /// Finder used to compare Widget with golden record
   Finder finder;
+
+  /// Parameter value (can be anything)
   late dynamic value;
+
+  /// Golden file name used as reference to compare with Widget
   late String golden;
 
   EveryTestFinder(this.finder);
 
+  /// Util method used to store param and golden file name
   EveryTestFinder matches(dynamic param, String goldenFile) {
     value = param;
     golden = goldenFile;
@@ -20,8 +28,11 @@ class EveryTestFinder {
   }
 }
 
+/// Util method used to instantiate a new Finder with the corresponding parameter
 EveryTestFinder finder(Finder any) => EveryTestFinder(any);
 
+/// Entry point to create a new 'everyTestGloden' series
+/// Uses a builder to store every arguments before run the tests
 @isTestGroup
 EveryTestGoldenBuilder everyTestGolden(
   String description, {
@@ -33,19 +44,34 @@ EveryTestGoldenBuilder everyTestGolden(
       .._expects(expects)
       .._run();
 
+/// Builder class handling every parameters in order to build and run tests
 class EveryTestGoldenBuilder {
+  /// Description of the test group
   final String description;
+
+  /// Callback containing the test to run for each parameter value
   late EveryTestGoldenCallback _callback;
+
+  /// List of finders used to match with golden
   late List<Finder> _finders;
+
+  /// List of parameters used to run tests
+  /// Each parameter in the list will trigger a new test instance to run
   late List<dynamic> _params;
+
+  /// Expected values for each parameter value
+  /// Each expected value is compared to the result of each test instance
   late List<String> _expecteds;
 
   EveryTestGoldenBuilder(this.description);
 
+  /// Util method used to specify test callback to invoke during the test
   void _of(EveryTestGoldenCallback callback) {
     _callback = callback;
   }
 
+  /// Util method used to specify each resul for every test instance
+  ///   depending on the supplied parameters
   void _expects(List<EveryTestFinder> finders) {
     _finders = [];
     _params = [];
@@ -57,6 +83,8 @@ class EveryTestGoldenBuilder {
     }
   }
 
+  /// Perform a run of the callback in a test for every parameter
+  ///   and compares the result to the expected value supplied
   void _run() {
     final findersIterator = _finders.iterator;
     final paramsIterator = _params.iterator;
--- lib/unit/unit_test.dart
+++ lib/unit/unit_test.dart
@@ -1,24 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 
+/// Callback type used to call the test code
 typedef EveryTestCallback = dynamic Function(
   dynamic parameter,
 );
 
+/// Symbolize a test parameter instance and its expected value
 class EveryTestParameter {
+  /// Parameter value (can be anything)
   dynamic value;
+
+  /// Expected value for value parameter supplied
   dynamic expected;
 
   EveryTestParameter(this.value);
 
+  /// method used to specify which value is expected for the specified parameter
   EveryTestParameter gives(dynamic expectedValue) {
     expected = expectedValue;
     return this;
   }
 }
 
+/// Util method used to instantiate a parameter instance
 EveryTestParameter param(dynamic any) => EveryTestParameter(any);
 
+/// Entry point to create a new 'everytest' series
+/// Uses a builder to store every arguments before run the tests
 @isTestGroup
 EveryTestBuilder everyTest(
   String description, {
@@ -30,18 +39,31 @@ EveryTestBuilder everyTest(
       .._expects(expects)
       .._run();
 
+/// Builder class handling every parameters in order to build and run tests
 class EveryTestBuilder {
+  /// Description of the test group
   final String description;
+
+  /// Callback containing the test to run for each parameter value
   late EveryTestCallback _callback;
+
+  /// List of parameters used to run tests
+  /// Each parameter in the list will trigger a new test instance to run
   late List<dynamic> _params;
+
+  /// Expected values for each parameter value
+  /// Each expected value is compared to the result of each test instance
   late List<dynamic> _expecteds;
 
   EveryTestBuilder(this.description);
 
+  /// Util method used to specify test callback to invoke during the test
   void _of(EveryTestCallback callback) {
     _callback = callback;
   }
 
+  /// Util method used to specify each resul for every test instance
+  ///   depending on the supplied parameters
   void _expects(List<EveryTestParameter> parameters) {
     _params = [];
     _expecteds = [];
@@ -51,6 +73,8 @@ class EveryTestBuilder {
     }
   }
 
+  /// Perform a run of the callback in a test for every parameter
+  ///   and compares the result to the expected value supplied
   void _run() {
     final paramsIterator = _params.iterator;
     final expectedsIterator = _expecteds.iterator;
